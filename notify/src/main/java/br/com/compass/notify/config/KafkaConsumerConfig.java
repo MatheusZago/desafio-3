6c4d51bf93cb2e18543b54f8d3c90baf54fbea47
Matheus <matheuszago134@gmail.com>
2024-10-29 12:55:26 -0300

    Add communication with kafka notify side

--- notify/src/main/java/br/com/compass/notify/config/KafkaConsumerConfig.java
+++ notify/src/main/java/br/com/compass/notify/config/KafkaConsumerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -18,20 +19,18 @@ import java.util.Map;
 public class KafkaConsumerConfig {
 
     @Bean
-    public DefaultKafkaConsumerFactory<String, NotifyMessage> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, NotifyMessage.class.getName()); // Mapeia o JSON para NotifyMessage
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "br.com.compass.api.model"); // Adiciona o pacote confiável
-        return new DefaultKafkaConsumerFactory<>(props);
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "notify-group");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, NotifyMessage> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, NotifyMessage> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
--- notify/src/main/java/br/com/compass/notify/service/NotifyService.java
+++ notify/src/main/java/br/com/compass/notify/service/NotifyService.java
@@ -7,17 +7,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotifyService {
 
-    //Ele ta ouvindo o que no grupo e tópico
     @KafkaListener(topics = "notify_topic", groupId = "notify-group")
-    public void listen(NotifyMessage message) {
-//        String logMessage = String.format("Received %s operation for user: %s", message.getOperation(), message.getUsername());
-//        System.out.println(logMessage);
-
-        // Exemplo de log variado
-        if ("CREATE".equals(message.getOperation())) {
-            System.out.println(message.getOperation() + " user: " + message.getUsername());
-        } else if ("UPDATE".equals(message.getOperation())) {
-            System.out.println(message.getOperation() + " user: " + message.getUsername());
-        }
+    public void listen(String message) {
+        // Processar a mensagem JSON como String
+        System.out.println("Received message: " + message);
     }
 }
